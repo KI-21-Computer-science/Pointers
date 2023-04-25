fa2b9b4743a7dec46c0fcb05854a33b2e8e6d051
Bohdan Kolomeiets <bod9i@outlook.com>
2023-04-25 15:23:26 +0300

    Add project files.

---
+++ Pointers/main.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+
+int Sum(int* array, int size)
+{
+    int result = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        result += array[i];
+    }
+
+    return result;
+}
+
+int* Modify(int* array, int size)
+{
+    int* newArray = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = array[i] * 2;
+    }
+
+    return newArray;
+}
+
+int main()
+{
+    int* p = new int { 10 }; // создание объекта типа int в куче через оператор new
+
+    int* array = nullptr;
+    int n;
+
+    std::cout << "Input count of elements: ";
+    std::cin >> n;
+
+    array = new int[n]; // создание динамического массива
+
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = rand() % 10;
+    }
+
+    // получение указателя на новый массив из функции 
+    int* newArray = Modify(array, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << newArray[i] << " ";
+    }
+
+    // очистка памяти для объекта НЕ массива и зануление указателя
+    delete p;
+    p = nullptr;
+
+    // очистка памяти для объекта массива и зануление указателя
+    delete[] newArray;
+    newArray = nullptr;
+
+    // создание массива символов, инициализация его строковым литералом
+    // можно с таким массивом работать как с обычной строкой (string)
+    char* char_array = new char[13] { "Hello world!" };
+    // в динамической памяти можно инициализировать объекты любого типа
+
+    return 0;
+}
